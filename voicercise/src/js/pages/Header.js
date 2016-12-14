8e631599a50ee8e28ee71de17a21cbef6b72833e
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-12-13 21:08:28 -0500

    adding profilePage registrationPAge

--- voicercise/src/js/pages/Header.js
+++ voicercise/src/js/pages/Header.js
@@ -10,8 +10,24 @@ export default class Header extends React.Componet {
 			  <div id='navbar-collapse' className= 'collapse navbar-collapse'>
 			   <ul className='nav navbar-nav'> 
 			    <li><Link to="/">Home</Link></li>
+			    <Authenticated>
+			     <li>
+			       <Link to='/profile'>Profile</Link>
+			     </li>
+			    </Authenticated>
 			   </ul>
 			   <ul className="nav navbar-nav navbar-right">
+			    <NotAuthenticated>
+			     <li>
+			      <LoginLink />
+			     </li>
+			    </NotAuthenticated>
+			    <NotAuthenticated>
+  				 <li>
+    			  <Link to="/register">Create Account</Link>
+  				 </li>
+			    </NotAuthenticated>
+
 			   </ul>
 			  </div>
 			 </div>
--- voicercise/src/js/pages/IndexPage.js
+++ voicercise/src/js/pages/IndexPage.js
@@ -22,5 +22,4 @@ export default class IndexPage extends React.Component {
 			</div>
 		);
 	}
-
 }
